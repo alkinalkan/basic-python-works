40881ce7c6bcd4c0ff5d47293ca49bfa02abd8c1
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-05-23 23:57:13 +0300

    Update basiclogin.py

--- basiclogin.py
+++ basiclogin.py
@@ -21,12 +21,11 @@ while True:
         submit *= number
         print(submit)
     if operator == "!":
-        print("Factorial of number is:")
         old_submit = submit
         for i in range(1, submit + 1):
             fact = fact * i
             submit = fact
-        print("{}! = {}".format(old_submit, submit))
+        print("Factorial of {} is:{}".format(old_submit, submit))
         fact = 1
     if operator == "q":
         print("Thanks for using calculator.\nYour last output: {}".format(submit))
