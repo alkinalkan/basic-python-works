42dbc3739983c4cff2dbe84409f4f629a1370f5a
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-05-27 21:47:02 +0300

    Log Changes basiclogin.py

--- basiclogin.py
+++ basiclogin.py
@@ -1,27 +1,27 @@
-submit = int(input("Hello! Welcome to Calculator. Please input a number:"))
+submit = int(input("Hello! Welcome to Calculator Program. Please input a number:"))
 fact = 1
 
 while True:
-    operator = input("Choose operator: + - / * ! q\n")
+    operator = input("Please choose operator: + - / * ! q\n")
     if operator == "+":
-        number = int(input("Please input a number:"))
+        number = int(input("Please input a number for summation:"))
         submit += number
         print(submit)
     if operator == "-":
-        number = int(input("Please input a number:"))
+        number = int(input("Please input a number for extraction:"))
         submit -= number
         print(submit)
     if operator == "/":
-        number = int(input("Please input a number:"))
+        number = int(input("Please input a number for division:"))
         submit /= number
         submit = int(submit)
         print(submit)
     if operator == "*":
-        number = int(input("Please input a number:"))
+        number = int(input("Please input a number for multipication:"))
         submit *= number
         print(submit)
     if operator == "!":
-        print("Factorial of number is:")
+        print("Please input a number for factorial operation:")
         old_submit = submit
         for i in range(1, submit + 1):
             fact = fact * i
