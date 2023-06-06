bb8b08d0f6660a62c2a3a1bf3b8a242567ce81a7
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-06-06 19:08:12 +0300

    Update basiclogin.py

--- basiclogin.py
+++ basiclogin.py
@@ -1,36 +1,52 @@
 # Python Program for calculation operations.
-# It includes summation, extraction, division, multipication and factorial.
+# It includes summation, subtraction, division, multiplication, exponentiation, modulus, and factorial.
 
-submit = int(input("Hello! Welcome to Calculator Program. Please input a number:"))
+submit = int(input("Hello! Welcome to the Calculator Program. Please input a number: "))
 fact = 1
 
 while True:
-    operator = input("Please choose operator: + - / * ! q\n")
+    operator = input("Please choose an operator: + - / * ^ % ! q\n")
+    
     if operator == "+":
-        number = int(input("Please input a number for summation:"))
+        number = int(input("Please input a number for summation: "))
         submit += number
         print(submit)
-    if operator == "-":
-        number = int(input("Please input a number for extraction:"))
+        
+    elif operator == "-":
+        number = int(input("Please input a number for subtraction: "))
         submit -= number
         print(submit)
-    if operator == "/":
-        number = int(input("Please input a number for division:"))
+        
+    elif operator == "/":
+        number = int(input("Please input a number for division: "))
         submit /= number
         submit = int(submit)
         print(submit)
-    if operator == "*":
-        number = int(input("Please input a number for multipication:"))
+        
+    elif operator == "*":
+        number = int(input("Please input a number for multiplication: "))
         submit *= number
         print(submit)
-    if operator == "!":
-        print("Please input a number for factorial operation:")
+        
+    elif operator == "^":
+        number = int(input("Please input a number for exponentiation: "))
+        submit **= number
+        print(submit)
+        
+    elif operator == "%":
+        number = int(input("Please input a number for modulus: "))
+        submit %= number
+        print(submit)
+        
+    elif operator == "!":
+        print("Please input a number for factorial operation: ")
         old_submit = submit
         for i in range(1, submit + 1):
-            fact = fact * i
+            fact *= i
             submit = fact
         print("{}! = {}".format(old_submit, submit))
         fact = 1
-    if operator == "q":
-        print("Thanks for using calculator.\nYour last output: {}".format(submit))
+        
+    elif operator == "q":
+        print("Thanks for using the calculator.\nYour last output: {}".format(submit))
         break
